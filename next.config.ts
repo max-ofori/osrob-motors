ae892ac35660ffed883106d6a8792de2366d2518
Maxwell Ofori <maxwell.ofori002@gmail.com>
2026-09-15 03:31:04 +0000

    allow supabase image domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+import type { NextConfig } from "next"; 
+const nextConfig: NextConfig = { images: { remotePatterns: [ { protocol: "https", hostname: "*.supabase.co", pathname: "/storage/v1/object/public/**", }, ], }, }; 
 
 export default nextConfig;
